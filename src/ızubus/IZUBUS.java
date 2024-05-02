29d602f899934b68771930a29ad5cfbf3e8688ff
FfatihErdem <030121060@std.izu.edu.tr>
2024-05-02 22:42:32 +0300

    Üniversite Servis Uygulaması

---
+++ src/ızubus/IZUBUS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ızubus;
+
+/**
+ *
+ * @author 90541
+ */
+public class IZUBUS {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Giris s = new Giris();
+        s.setVisible(true);
+    }
+    
+}
